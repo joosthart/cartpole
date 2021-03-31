a1c56a8ab2f4fbec1e0be0588b97cab9b2094860
joosthart <hartjoost@gmail.com>
2021-03-31 17:06:35 +0200

    comments in agents (1/2)

--- src/dql/agent.py
+++ src/dql/agent.py
@@ -10,7 +10,8 @@ from src.dql.model import DenseModel
 SEED = 42
 
 class ExperienceBuffer:
-
+    """Experience Buffer used for experience replay by DeepQLearning
+    """
     def __init__(self, max_buffer_size, obs_space):
         self.max_buffer_size = int(max_buffer_size)
         self.obs_space = int(obs_space)
@@ -25,6 +26,8 @@ class ExperienceBuffer:
         self.size = 0
 
     def add(self, state, action, reward, next_state, done_flag):
+        """Add a state to buffer
+        """
         self.states[self.row] = state
         self.actions[self.row] = action
         self.rewards[self.row] = reward
@@ -35,9 +38,19 @@ class ExperienceBuffer:
         self.row = (self.row + 1) % self.max_buffer_size
     
     def get_size(self):
+        """Get the current size of the buffer
+        """
         return self.size
 
     def get_batch(self, batch_size):
+        """Get a random batch from the buffer
+        """
+        if batch_size > self.get_size:
+            raise ValueError(
+                'Batch size is too large. Batch size should be less than or '
+                'equal to current buffer size.'
+            )
+
         idx = np.random.choice(
             min(self.size, self.max_buffer_size), batch_size, replace=False
         )
@@ -50,7 +63,8 @@ class ExperienceBuffer:
         )
 
 class DeepQLearning:
-
+    """Deep Q-Learning agent for solving the CartPole problem.
+    """
     def __init__(
             self, num_hidden_states=[8,8], discount=0.95, max_buffer_size=1e6, 
             min_buffer_size=1e3, batch_size=64, lr=0.01, update_freq=10, 
@@ -58,8 +72,9 @@ class DeepQLearning:
             hidden_activation='relu', hidden_initializer='random_normal', 
             output_initializer='random_normal', log_dir='./log/'):
 
+        # init environment
         self.env = gym.make('CartPole-v0')
-        
+        # Set random seeds
         self.env.seed(SEED)
         tf.random.set_seed(SEED)
         np.random.seed(SEED)
@@ -76,6 +91,7 @@ class DeepQLearning:
         self.epsilon_decay = epsilon_decay
         self.min_epsilon = min_epsilon
 
+        # Initialize Neural Networks
         self.model = DenseModel(
             input_shape=self.num_states,
             output_shape=self.num_actions,
@@ -96,6 +112,7 @@ class DeepQLearning:
             output_initializer=output_initializer
         )
 
+        # Initialize tensorboard
         self.summary_writer = tf.summary.create_file_writer(log_dir)
 
         if self.min_buffer_size < self.batch_size:
@@ -104,12 +121,26 @@ class DeepQLearning:
                 'size.'
             ))
         
+        # Initialize buffer
         self.memory = ExperienceBuffer(self.max_buffer_size, self.num_states)
 
     def _update_epsilon(self):
+        """ Update epsilon with epsilon decay. If new epsilon is smaller than 
+        minimal epsilon, returns minimal epsilon.
+        """
         self.epsilon = max(self.epsilon * self.epsilon_decay, self.min_epsilon)
 
     def _get_action(self, state, simulate=False):
+        """Get an action for a given state.
+
+        Args:
+            state (numpy.array): environment current observation state
+            simulate (bool, optional): If false, use epsilon-greedy 
+                exploitation. Else 100% exploitation. Defaults to False.
+
+        Returns:
+            int: action
+        """
         # Explore
         if np.random.random() < self.epsilon and not simulate:
             return self.env.action_space.sample()
@@ -117,30 +148,46 @@ class DeepQLearning:
         else:
             return np.argmax(self.model.predict(state), axis=1)[0]
 
-    def _bellman(self, next_q, reward):
-        return reward + self.discount*next_q
-
     def _update_model(self):
+        """Update target model with training model weights.
+        """
         self.target_model.set_weights(self.model.get_weights())
 
     def _train_one_batch(self):
+        """Train on one batch of data. In this method the target values are 
+        calculated and the training is started.
 
+        Returns:
+            float: Traing loss
+        """
+        # Get a batch of data
         states, actions, rewards, next_states, done_flags = \
             self.memory.get_batch(self.batch_size)
 
+        # Get current state Q-values from trainig model
         q_values = self.model.predict(states)
+        # Get target Q-values using target model
         q_values_next = self.target_model.predict(next_states)
 
+        # Calculate target Q-values using Bellman's equation
         q_values[range(len(q_values)), actions] = \
             rewards + self.discount*np.max(q_values_next, axis=1)
         q_values[done_flags, actions[done_flags]] = rewards[done_flags]
 
+        # Train train model on one batch of data
         loss = self.model.train(states, q_values)
         
         return loss
 
     def train(self, num_epochs, verbose=True, render_every=500):
+        """Main training method
 
+        Args:
+            num_epochs (int): Number of epochs to train for
+            verbose (bool, optional): Verbose tag. Defaults to True.
+            render_every (int, optional): Frequency to display the algorith 
+                performace visiually. Defaults to 500.
+        """
         self.total_training_reward = []
         self.total_training_loss = []
         self.total_epsilon = []
@@ -150,19 +197,23 @@ class DeepQLearning:
         else:
             pbar = range(num_epochs)
         for epoch in pbar:
+            # Reset environment
             state = self.env.reset()
-            done = False
+
             epoch_reward = 0
+            done = False
             epoch_loss = []
             while not done:
                 if verbose and epoch != 0 and epoch % render_every == 0:
                     self.env.render()
-                action = self._get_action(state)
 
+                # get next action
+                action = self._get_action(state)
+                # make move
                 next_state, reward, done, _ = self.env.step(action)
 
                 epoch_reward += reward
-
+                # Add state to experience buffer
                 self.memory.add(
                     state=state, 
                     action=action, 
@@ -170,7 +221,7 @@ class DeepQLearning:
                     next_state=next_state,
                     done_flag=done
                 )
-
+                # If experience buffer large enough, train on one batch of data.
                 if self.memory.get_size() > self.min_buffer_size:
                     loss = self._train_one_batch()
                     epoch_loss.append(loss) 
@@ -180,11 +231,13 @@ class DeepQLearning:
             self.total_training_reward.append(epoch_reward)
             self.total_epsilon.append(self.epsilon)
 
+            # Update epsilon
             if self.memory.get_size() > self.min_buffer_size:
                 self._update_epsilon()
 
             self.total_training_loss.append(epoch_loss)
             
+            # Write performance to Tensorboard
             with self.summary_writer.as_default():
                 tf.summary.scalar(
                     'epoch reward', 
@@ -221,13 +274,32 @@ class DeepQLearning:
                 })
 
     def save(self, fn):
+        """Save model
+
+        Args:
+            fn (str): path to save model
+        """
         self.model.save(fn)
 
     def load(self, fn):
+        """Load model
+
+        Args:
+            fn (str): path to save model
+        """
         self.model.load(fn)
 
     def simulate(self, n_simulations, verbose=True, maxsteps=500):
+        """Simualte the current model performance visually
 
+        Args:
+            n_simulations (int): Number of simulations
+            verbose (bool, optional): Vebose flag. Defaults to True.
+            maxsteps (int, optional): Maximal steps of episode. Defaults to 500.
+
+        Returns:
+            list[int]: obtained rewards
+        """
         rewards = []
         for i in range(n_simulations):
             # Reset environment
--- src/tql/agent.py
+++ src/tql/agent.py
@@ -9,7 +9,8 @@ import tensorflow as tf
 SEED = 42
 
 class TabularQLearning:
-
+    """Tabular Q-Learning agent for solving the CartPole problem.
+    """
     def __init__(self, epsilon=1, discount=0.95, lr=0.1, epsilon_decay=0.999, 
                  save_progress=True, log_dir='log_tql', **kwargs):
         self.epsilon = epsilon
@@ -27,21 +28,44 @@ class TabularQLearning:
         ]
 
         self.save_progress = save_progress
+        # Initialize tensorboard
         if self.save_progress:
             self.summary_writer = tf.summary.create_file_writer(log_dir)
 
+        # Init env
         self.env = gym.make('CartPole-v0')
         self.env.seed(SEED)
+        
         self.table = {}
 
+        # set numpy seed
         np.random.seed(SEED)
 
     def _bellman(self, state, next_state, action, reward):
+        """Calculate Q-values using Bellman's equation to update table.
+
+        Args:
+            state (numpy.array): environment current observation state
+            next_state (numpy.array): environment next state observation
+            action (int): Move taken
+            reward (int): Reward for action at current state
+
+        Returns:
+            float: Q-value
+        """
         q0 = self._get_q_value(state, action)
         q1 = self._get_q_value(next_state, self._get_best_action(next_state))
         return (1-self.lr)*q0 + self.lr*(reward + self.discount*q1)
 
     def _discretize(self, state):
+        """Discretize state
+
+        Args:
+            state (numpy.array): environment current observation state
+
+        Returns:
+            numpy.array: Discretized state
+        """
         state = state.copy()
         for idx, (s, ds) in enumerate(zip(state, self.step_size)):
             state[idx] = round(
@@ -51,6 +75,11 @@ class TabularQLearning:
         return state
 
     def _add_state(self, state):
+        """Add a state tot the Q-learning table
+
+        Args:
+            state (numpy.array): environment current observation state
+        """
         action_space = list(range(self.env.action_space.n))
         random.shuffle(action_space)  # Random action is the first in the dict
         self.table[tuple(state)] = {
@@ -58,22 +87,40 @@ class TabularQLearning:
         }
 
     def _set_q_value(self, state, action, score):
+        """ Set Q-value to in table
+        """
         self.table[tuple(state)][action] = score
 
     def _get_q_value(self, state, action):
+        """ Add Q-value in table
+        """
         return self.table[tuple(state)][action]
 
     def _get_best_action(self, state):
+        """ Get action with highest Q-value for a given state
+        """
         return max(
             self.table[tuple(state)],
             key=self.table[tuple(state)].get
         )
 
-    def _update_epsilon(self, epoch):
+    def _update_epsilon(self):
+        """ Update epsilon with epsilon decay. If new epsilon is smaller than 
+        minimal epsilon, returns minimal epsilon.
+        """
         self.epsilon = max(self.min_epsilon, self.epsilon*self.epsilon_decay)
 
     def train(self, epochs, maxsteps=10000, render_every=500, verbose=True):
+        """ Main training method
 
+        Args:
+            epochs (int): Number of Epochs to train for
+            maxsteps (int, optional): Maximal environment steps. Defaults to 
+                10000.
+            render_every (int, optional): Frequency to display the algorith 
+                performace visiually. Defaults to 500.
+            verbose (bool, optional): Verbose flag. Defaults to True.
+        """
         self.total_training_reward = []
         self.total_epsilon = []
 
@@ -81,8 +128,8 @@ class TabularQLearning:
             pbar = tqdm(range(epochs))
         else:
             pbar = range(epochs)
+        
         for i in pbar:
-            
             # Reset environment
             state = self.env.reset()
 
@@ -99,6 +146,7 @@ class TabularQLearning:
                 if verbose and i != 0 and i % render_every == 0:
                     self.env.render()
 
+                # Epsilon-greedy exploration implementation
                 # Exploit
                 if random.uniform(0, 1) > self.epsilon:
                     if tuple(state_disc) not in self.table.keys():
@@ -109,30 +157,35 @@ class TabularQLearning:
                 else:
                     action = self.env.action_space.sample()
 
+                # make move
                 next_state, reward, done, _ = self.env.step(action)
-
+                # Discretize state
                 next_state_disc = self._discretize(next_state)
                 if tuple(next_state_disc) not in self.table.keys():
                     self._add_state(next_state_disc)
 
+                # Calculate new Q-value using bellman's equation
                 new_score = self._bellman(
                     state_disc,
                     next_state_disc,
                     action,
                     reward
                 )
-
+                
+                # Add new Q-value to table
                 self._set_q_value(state_disc, action, new_score)
 
                 epoch_reward += reward
                 state_disc = next_state_disc
                 step += 1
 
+            # Update epsilon
             self._update_epsilon(i)
 
             self.total_training_reward.append(epoch_reward)
             self.total_epsilon.append(self.epsilon)
 
+            # Write performance to Tensorboard
             if self.save_progress:
                 with self.summary_writer.as_default():
                     tf.summary.scalar(
@@ -155,6 +208,11 @@ class TabularQLearning:
                 })
 
     def save(self, fn):
+        """Save model
+
+        Args:
+            fn (str): path to save model
+        """
         savedict = self.__dict__.copy()
         savedict.pop('env')
 
@@ -162,30 +220,49 @@ class TabularQLearning:
             pickle.dump(savedict, f)
 
     def load(self, fn):
+        """Load model
+
+        Args:
+            fn (str): path to save model
+        """
         with open(fn, 'rb') as f:
             loaddict = pickle.load(f)
 
         for k, v in loaddict.items():
             setattr(self, k, v)
 
-    def simulate(self, maxsteps=100):
-        done = False
-        step = 0
+    def simulate(self, n_simulations, verbose=True, maxsteps=500):
+        """Simualte the current model performance visually
 
-        # Reset environment
-        state = self.env.reset()
-        # Discretize state
-        state_disc = self._discretize(state)
-        while not done and step < maxsteps:
+        Args:
+            n_simulations (int): Number of simulations
+            verbose (bool, optional): Vebose flag. Defaults to True.
+            maxsteps (int, optional): Maximal steps of episode. Defaults to 500.
 
-            self.env.render()
-
-            if tuple(state_disc) in self.table.keys():
-                action = self._get_best_action(state_disc)
-            else:
-                action = self.env.action_space.sample()
-
-            next_state, _, done, _ = self.env.step(action)
-
-            state_disc = self._discretize(next_state)
-            step += 1
+        Returns:
+            list[int]: obtained rewards
+        """
+        rewards = []
+        for i in range(n_simulations):
+            # Reset environment
+            state = self.env.reset()
+            # Discretize state
+            state_disc = self._discretize(state)
+            done = False
+            step = 0
+            sim_reward = 0
+            while not done and step < maxsteps:
+                if verbose:
+                    self.env.render()
+                if tuple(state_disc) in self.table.keys():
+                    action = self._get_best_action(state_disc)
+                else:
+                    action = self.env.action_space.sample()
+                next_state, reward, done, _ = self.env.step(action)
+                state_disc = self._discretize(next_state)
+                sim_reward += reward
+                step += 1
+            if verbose:          
+                print('Reward run {}: {}'.format(i+1, sim_reward))
+            rewards.append(reward)
+        return rewards
